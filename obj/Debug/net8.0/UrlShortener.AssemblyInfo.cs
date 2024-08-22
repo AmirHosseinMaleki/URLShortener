d73596277deb0d5e7683924e539c2d4d3bd23400
Amir Maleki <40566488+AmirHosseinMaleki@users.noreply.github.com>
2024-08-22 17:04:38 +0200

    Apply Migrations and Update the Database

--- obj/Debug/net8.0/UrlShortener.AssemblyInfo.cs
+++ obj/Debug/net8.0/UrlShortener.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UrlShortener")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22c7067572132e10f83d0cbb9372426a86ab62fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3c95f5b0b411976a5611e972f932d92fdcb25fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("UrlShortener")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UrlShortener")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
