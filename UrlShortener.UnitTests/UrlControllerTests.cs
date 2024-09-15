93ada801a08fed6e163024b9fae546be5ce2a03f
Amir Maleki <40566488+AmirHosseinMaleki@users.noreply.github.com>
2024-09-15 13:49:58 +0200

    Add UnitTests for GetAll() method

--- UrlShortener.UnitTests/UrlControllerTests.cs
+++ UrlShortener.UnitTests/UrlControllerTests.cs
@@ -5,6 +5,7 @@ using UrlShortener.Models;
 using Microsoft.EntityFrameworkCore;
 using Xunit;
 using UrlShortener.UnitTests;
+using UrlShortener.ViewModels;
 
 namespace UrlShortener.UrlShortener.UnitTests;
 
@@ -39,5 +40,23 @@ public class UrlControllerTests
         var okResult = Assert.IsType<OkObjectResult>(result.Result);
         var returnUrls = Assert.IsType<List<Url>>(okResult.Value);
         Assert.NotNull(returnUrls);
+    }
+
+    [Fact]
+    public async Task GetById_ShouldReturnOkResult_WithUrl()
+    {
+        var result = await controller.GetById(1);
+
+        var okResult = Assert.IsType<OkObjectResult>(result);
+        var returnUrl = Assert.IsType<Url>(okResult.Value);
+        Assert.Equal(1, returnUrl.Id);
+    }
+
+    [Fact]
+    public async Task GetById_ShouldReturnNotFound_WhenUrlDoesNotExist()
+    {
+        var result = await controller.GetById(999);
+
+        Assert.IsType<NotFoundResult>(result);
     }
 }
